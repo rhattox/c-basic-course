a21192c4a7d528c134330f7cf03759503662b8a8
Bruno Souto <bcovies@gmail.com>
2023-11-30 17:10:58 -0300

    Add basic Hello World

---
+++ cmake-build-debug/CMakeFiles/c_basic_course.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_basic_course.dir/main.cpp.o"
+  "c_basic_course"
+  "c_basic_course.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_basic_course.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

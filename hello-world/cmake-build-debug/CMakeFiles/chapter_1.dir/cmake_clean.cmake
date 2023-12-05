a016cc3483e0f0ab38e3720aa281aebc5fbed6a6
Bruno Souto <65253099+bcovies@users.noreply.github.com>
2023-12-05 15:12:51 -0300

    Add hello world C

---
+++ hello-world/cmake-build-debug/CMakeFiles/chapter_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter_1.dir/main.c.o"
+  "chapter_1"
+  "chapter_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chapter_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
